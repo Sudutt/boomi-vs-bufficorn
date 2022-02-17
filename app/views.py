c0d677f09c4ccd7737e6c7d1bb12c7a922bc1646
Sudutt Harne <harne.sudutt11@gmail.com>
2022-02-18 03:07:16 +0530

    corrected / and /arena. Added style for /.

--- app/views.py
+++ app/views.py
@@ -4,4 +4,8 @@ from . import app
 
 @app.route('/', methods=['GET'])
 def index():
+    return render_template('entry.html')
+
+@app.route('/arena', methods=['GET'])
+def entry():
     return render_template('index.html')
